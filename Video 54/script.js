b99f41991f288622939744fe5441c0de412c46d9
itsaivivek <er.vivek.social@gmail.com>
2025-06-11 21:00:06 +0545

    Tutorial 54 - Introduction to Javascript and Installing Node.js

---
+++ Video 54/script.js
@@ -0,0 +1,20 @@
+ alert("Hello World");
+
+ console.log("Code is running...")
+ console.log("Code is also running...")
+ console.log("Code is looking like a wow...")
+
+ var a = prompt("Enter your Number")
+ var isTrue = confirm("Are you sure want to leave this page and blast your computer ")
+
+if(isTrue){
+    console.log("Your computer is blasting")
+}
+else{
+    console.log("Your computer is not blasting")
+}
+ console.log("Your number is " + a)
+
+ document.title = "Hey I am good"
+
+//  document.body.style.backgroundColor = "red"
