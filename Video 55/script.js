42fd85184cb0b91c720157fc5ebc2063df5fd970
itsaivivek <er.vivek.social@gmail.com>
2025-06-12 09:08:50 +0545

    Tutorial 55- Javascript Variables,Datatypes and Objects

---
+++ Video 55/script.js
@@ -0,0 +1,46 @@
+console.log("Hey this is tutorial 55")
+let a = 5;
+// a = a + 1;
+// var a = 5;
+// a = a+1;
+// var b = 6;
+// var c = "Harry";
+// var _a = "Shubham";
+
+// var 55a = "Rohan"; This is invalid
+
+// console.log(a + b + 8)
+// console.log(typeof a, typeof b, typeof c)
+{
+    let a = 66;
+    console.log(a);
+}
+console.log(a)
+
+// const a1 = 6;
+// a1 = a1+1; this is invalid because a1 is constant
+
+// var is globally scope and let is block scope
+
+let x = "Harry bhai";
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+
+let o = {
+    // we can add name into double quote also but double code is for multi words 
+    name: "Harry",
+    "job code": 5600,
+    "is_handsome": true,
+} 
+console.log(o);
+o.salary = "100crores";
+console.log(o);
+o.salary = "500crores";
+console.log(o);
