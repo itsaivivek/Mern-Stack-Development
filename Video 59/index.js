260808061fc21f340cae1b0d6e625b816b86841f
itsaivivek <er.vivek.social@gmail.com>
2025-06-16 08:53:56 +0545

    Exercise - Faulty Calculator

--- Video 59/index.js
+++ Video 59/index.js
@@ -11,7 +11,7 @@ It performs wrong operation 10% of the times.
 
 // This can generate random number from 0 to 1
 random = Math.random();
-console.log(random);
+// console.log(random);
 
 // prompt only works in browser
 // let a = prompt("Enter number a ");
@@ -20,17 +20,32 @@ let a = 5;
 let b = 4;
 
 if (random<0.1) {
-    console.log("The addition of a and b is", (a-b))
+    console.log("The addition of a and b is", diff(a,b))
     // console.log("The addition of a and b is "+ (a+b)) //Both are same but + is used for string concatenate and comma for number it automatically add space before.
 
-    console.log("The subtraction of a and b is", (a/b))
-    console.log("The multipliction of a and b is", (a+b))
-    console.log("The division of a and b is", (a**b))
+    console.log("The subtraction of a and b is", division(a,b))
+    console.log("The multipliction of a and b is", sum(a,b))
+    console.log("The division of a and b is", expo(a,b))
     
 }
 else{
-    console.log("The addition of a and b is", (a+b))
-    console.log("The subtraction of a and b is", (a-b))
-    console.log("The multipliction of a and b is", (a*b))
-    console.log("The division of a and b is", (a/b))
+    console.log("The addition of a and b is", sum(a,b))
+    console.log("The subtraction of a and b is", diff(a,b))
+    console.log("The multipliction of a and b is", product(a,b))
+    console.log("The division of a and b is", division(a,b))
+}
+function sum(a, b) {
+    return(a+b);
+}
+function diff(a, b) {
+    return(a-b);
+}
+function product(a, b) {
+    return(a*b);
+}
+function division(a, b) {
+    return(a/b);
+}
+function expo(a, b) {
+    return(a**b);
 }
