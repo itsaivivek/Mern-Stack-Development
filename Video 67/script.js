e58a47b3c7a7baf18d4788f271a4b7268f82f8fc
itsaivivek <er.vivek.social@gmail.com>
2025-06-18 11:16:54 +0545

    Tutorial 67 - JS DOM-Children, Parent & Sibling Nodes

---
+++ Video 67/script.js
@@ -0,0 +1,14 @@
+console.log("Hello World")
+
+document.body.childNodes
+document.body.childNodes[1]
+document.body.firstChild
+document.body.lastChild
+document.body.firstElementChild
+document.body.firstElementChild.childNodes
+document.body.firstElementChild.children
+document.body.firstElementChild.children[3].nextElementSibling.style.color = "red";
+document.body.firstElementChild.children[3].previousElementSibling
+document.body.firstElementChild.children[3].previousElementSibling.parentElement
+
+document.body.children[1].rows
