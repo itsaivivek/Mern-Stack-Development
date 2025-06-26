d78a0a50fbb0fbf998f27d04987491943c0262d6
itsaivivek <er.vivek.social@gmail.com>
2025-06-26 09:10:28 +0545

    Inserting & Removing Elements using JS

---
+++ Video 71/script.js
@@ -0,0 +1,16 @@
+console.log(document.querySelector(".box").innerHTML)
+console.log(document.querySelector(".container").innerHTML)
+console.log(document.querySelector(".container").innerText)
+console.log(document.querySelector(".container").outerHTML)
+console.log(document.querySelector(".container").tagName)
+console.log(document.querySelector(".container").nodeName)
+console.log(document.querySelector(".container").textContent)
+// console.log(document.querySelector(".container").hidden = true)
+// document.querySelector(".box").innerHTML = "Hey I am Harry"
+console.log(document.querySelector(".box").hasAttribute("style"));
+console.log(document.querySelector(".box").getAttribute("style"))
+// document.querySelector(".box").setAttribute("style","display:inline;")
+console.log(document.querySelector(".box").attributes)
+// document.querySelector(".box").removeAttribute("style")
+// document.designMode = "on";
+console.log(document.querySelector(".box").dataset)
