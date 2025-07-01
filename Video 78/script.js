4660673d58b9e0e0b1efbcffe5e3db6d8babd457
itsaivivek <er.vivek.social@gmail.com>
2025-07-01 14:20:29 +0545

    Hacking Simulator Exercise

---
+++ Video 78/script.js
@@ -0,0 +1,37 @@
+let textarr = ["Initializing Hacking",
+    "Reading your Files",
+    "Password files Detected",
+    "Sending all passwords and personal files to server",
+    "Cleaning up"]
+let div = document.createElement("div")
+let i = 0
+let rand;
+getrandomDelay = () => {
+    rand = 1000* (1 + 6 * Math.random())
+    return rand;
+}
+
+console.log(rand);
+function inserting(textarr) {
+    let timerId = setInterval(() => {
+        div.innerHTML += `<p class = "text">\>\> ${textarr[i]}</p>`
+        document.body.append(div);
+        i++;
+        if(i == 5){
+            clearInterval(timerId)
+            setTimeout(() => {
+                
+                Array.from(div.children).forEach((e) => {
+                  e.classList.remove("text");
+                }
+                )
+            }, 1000);
+        }
+    }, (getrandomDelay()))
+
+}
+
+
+inserting(textarr)
+
+
